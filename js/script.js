d7ed2d8c51805b2297173bfb44578ff394295f41
Lucas Brandão <lucasbranndao@gmail.com>
2023-05-08 15:56:00 -0300

    commit end

--- js/script.js
+++ js/script.js
@@ -1,19 +1,39 @@
+import {Modal } from "./modal.js"
+import {AlertError } from "./alert-error.js"
+import { IMC, notANumber } from "./utils.js"
+
 const form = document.querySelector("form")
-const inputweight = document.querySelector("#peso")
-const inputHeight =document.querySelector("#altura")
-const botaoCalc = document.querySelector('.calcula-IMC')
+const inputWeight = document.querySelector("#weight")
+const inputHeight = document.querySelector("#height")
 
 form.onsubmit = (event) =>{
   event.preventDefault()
   
-  const weight = inputweight.value
+  const weight = inputWeight.value
   const height = inputHeight.value
 
- const result= IMC(weight, height)
- console.log(result)
+  const result = IMC(weight, height)
+  const message = `Seu IMC é de ${result}`
+  
+  const showAlertError = notANumber(weight) || notANumber(height)
+
+  if (showAlertError) {
+    AlertError.open()
+    return;
+  }
+  AlertError.close()
+
+  Modal.message.innerText = message
+  Modal.open()
+   
 }
 
-function IMC (weight, height)  {
-  return (weight / (height / 100) ** 2).toFixed(2)
+
+Modal.buttonClose.onclick = () => {
+  Modal.close()
 }
+
+
+
+
 
